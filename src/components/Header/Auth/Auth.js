e20d8756adff35adc455c528162d59476ebdf34c
SelinaIra <irra.selina@mail.ru>
2020-12-02 21:43:54 +0400

    logout + welcome message

--- src/components/Header/Auth/Auth.js
+++ src/components/Header/Auth/Auth.js
@@ -1,41 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import classnames from 'classnames/bind';
 import Modal from '../../Modal';
 import Form from './Form';
-import { userId } from '../../../redux/user';
+import { logOut, userLogin } from '../../../redux/user';
 
 import styles from './Auth.module.scss';
 
 const cx = classnames.bind(styles);
 
 function Auth() {
+  const dispatch = useDispatch();
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const handleClick = () => {
+
+  const handleLoginClick = () => {
     setModalIsOpen(!modalIsOpen);
   };
+  const handleLogoutClick = () => {
+    dispatch(logOut());
+  };
 
-  const userAuthorized = useSelector(userId);
+  const userAuthorized = useSelector(userLogin);
   useEffect(() => {
     if (userAuthorized) { setModalIsOpen(false); }
   }, [userAuthorized]);
+
   return (
     <>
+      {(!userAuthorized && (
+      <>
+        <button
+          type="button"
+          className={cx('auth__button')}
+          onClick={handleLoginClick}
+        >
+          Вход
+        </button>
+        <Modal
+          isOpen={modalIsOpen}
+          title="Вход"
+          onClose={() => setModalIsOpen(false)}
+        >
+          <Form />
+        </Modal>
+      </>
+      )) || (
       <button
         type="button"
         className={cx('auth__button')}
-        onClick={handleClick}
+        onClick={handleLogoutClick}
       >
-        Вход
-
+        Выход
       </button>
-      <Modal
-        isOpen={modalIsOpen}
-        title="Вход"
-        onClose={() => setModalIsOpen(false)}
-      >
-        <Form />
-      </Modal>
+      )}
     </>
   );
 }
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import classnames from 'classnames/bind';
+import { userLogin } from '../../redux/user';
+import styles from './App.module.scss';
+
+const cx = classnames.bind(styles);
 
 function App() {
+  const userName = useSelector(userLogin);
   return (
-    <div> App </div>
+    <div className={cx('home')}>
+      <i className={cx('home__title')}>
+        Привет,
+        {' '}
+        {userName || 'Гость'}
+        !
+      </i>
+    </div>
   );
 }
 
